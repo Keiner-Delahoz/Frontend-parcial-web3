1213c42ced6b68c9be7ebf8497a6bd08cbff9b9a
DanielMual <demunoz98@misena.edu.co>
2023-06-04 10:40:40 -0500

    Añadido Cuerpo Uno

--- src/components/cuerpo_uno/Cuerpo_uno.jsx
+++ src/components/cuerpo_uno/Cuerpo_uno.jsx
@@ -1,9 +1,33 @@
+import "../../styles/Cuerpo_uno.css";
+import foto1 from "../../assets/img/foto4.jpg";
 const Cuerpo_uno = () => {
-   return (
-      <div>
-         <h1>Hola Mundo</h1>
+  return (
+    <div className="elemenCuerpo">
+      
+      <div className="imgCuerpo">
+        <img src={foto1} className="d-block w-100" alt="..." />
       </div>
-   )
-}
+
+      <div>
+        <h1 className="nomDoc">Dr. Angelica Medina</h1>
+        <p className="parrafo">
+          ¡Hola! Soy la Dra. Angélica, una apasionada médica comprometida con
+          brindar atención de calidad y cuidado compasivo a mis pacientes. Me
+          gradué con honores de la Universidad de Medicina de renombre, donde
+          adquirí conocimientos sólidos y habilidades clínicas excepcionales.
+          Con más de una década de experiencia en medicina, he trabajado en
+          diversos entornos, desde hospitales de primer nivel hasta clínicas
+          comunitarias, lo que me ha permitido enfrentar una amplia gama de
+          casos médicos. Mi enfoque se centra en la medicina integral, poniendo
+          énfasis en la prevención y promoción de la salud. Como médica, mi
+          objetivo es ayudar a mis pacientes a alcanzar su máximo bienestar,
+          tanto físico como emocional. Estoy emocionada de colaborar con ustedes
+          y ser parte de su viaje hacia una vida saludable y feliz. ¡Juntos
+          podemos lograrlo!
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Cuerpo_uno;
