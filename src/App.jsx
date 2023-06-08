44bafdb9b4307fc92484d36a12541e6852958edc
Keiner De La Hoz <kfdelahoz@gmail.com>
2023-06-08 10:38:13 -0500

    ultimos detalles

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import './App.css'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer1'
 import { Routes, Route } from 'react-router-dom'
-// import Cuerpo_dos from './components/cuerpo_dos/Cuerpo_dos'
 import Home from './views/home'
 import Tables from './components/tables/Table'
-// import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
 
   return (
    <div>
-      
       <Header/>
       <Routes>
          <Route path='/'element={<Home/>}></Route>
